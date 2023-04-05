02f01587e3e77edd8447e6a39e33c964c8a09a62
Nigel Major <NigelWGMajor@users.noreply.github.com>
2023-04-05 15:34:49 -0500

    Sample range and date bindings on Settings page

--- WebApps/Emulator/Pages/Settings.cshtml.cs
+++ WebApps/Emulator/Pages/Settings.cshtml.cs
@@ -4,21 +4,45 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Emulator.Pages
 {
     public class SettingsModel : PageModel
-    {   public string PageName => "Settings";
+    {
+        public string PageName => "Settings";
+
         private readonly ILogger<SettingsModel> _logger;
+
+        private int _sliderValue;
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime DateValue1 { get; set;} = DateTime.Now;
+
+        [BindProperty(SupportsGet = true)]
+
+        public int SliderValue1 { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int SliderValue2 { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int SliderValue3 { get; set; }
 
         public SettingsModel(ILogger<SettingsModel> logger)
         {
             _logger = logger;
-        }    
+        }
+
         public void OnGet()
         {
-
+            SliderValue1 = 42;
+            SliderValue3 = 100 - 42;
         }
+
         public IActionResult OnPostSomething()
         {
             return RedirectToPage("/Error");
-        //    return Page();
+            //    return Page();
+        }
+
+        public void OnPost()
+        {
         }
     }
 }
