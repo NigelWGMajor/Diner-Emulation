916d9a6f062f78caa3c7002c5706102f98690a72
Nigel Major <NigelWGMajor@users.noreply.github.com>
2023-04-26 12:03:38 -0500

    Add Async

--- Imports/WorkflowManager/Services/DataService.cs
+++ Imports/WorkflowManager/Services/DataService.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Nix.Library.SqlStore;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,31 @@ using WorkflowManager.Models;
 
 namespace WorkflowManager.Services
 {
-    public interface IDataService
+    public interface IDataStore
     {
-
+        long Store<T>(string tableName, string identityName, T data);
+        T[] Recall<T>(string tableName, string identityName, long identity);
+        T[] Recall<T>(string tableName, string condition);
+        T[] Recall<T>(string sql);
+        Task<long> StoreAsync<T>(string tableName, string identityName, T data);
+        Task<T[]> RecallAsync<T>(string tableName, string identityName, long identity);
+        Task<T[]> RecallAsync<T>(string tableName, string condition);
+        Task<T[]> RecallAsync<T>(string sql);
     }
     public class DataStore : IDataStore
     {
-        // Simplified db access: shold be translatable for any db.
-        // You can insert one record into a table and get the id
-        // you can update a record if you know the id
-        // you can retrieve one record by id
-        // you can retrieve records where a condition is true
+        // Simplified db access Store or Recall <T>:
+        // 
+        // Identifiers shuold be long (bigint in sql)
+        //
+        // Store: You can insert an object into the store and get an identifier
+        //        You can update an object in the store if you know the identifier
+        //
+        // Recall: You can retrieve an object by identifier
+        //         You can retrieve objects matching some condition
+        //
+        // For store, the type must be a subset of the store columns
+        // For recall, only the matched properties are populated 
 
         private string _connectionString;
         // using Microsoft.Data.SqlClient;
@@ -75,7 +90,9 @@ namespace WorkflowManager.Services
                     {   // don't set the identity property!
                         continue;
                     }
-                    if (property.PropertyType == typeof(string))
+                    if (property.PropertyType == typeof(string)
+                        || property.PropertyType == typeof(bool)
+                        || property.PropertyType == typeof(DateTime))
                     {
                         if (initial)
                             sb.Append($"{property.Name}='{property.GetValue(data)}'");
@@ -104,7 +121,9 @@ namespace WorkflowManager.Services
                     {   // don't set the identity property!
                         continue;
                     }
-                    if (property.PropertyType == typeof(string))
+                    if (property.PropertyType == typeof(string)
+                        || property.PropertyType == typeof(bool)
+                        || property.PropertyType == typeof(DateTime))
                     {
                         if (initial)
                         {
@@ -153,6 +172,125 @@ namespace WorkflowManager.Services
                 }
             }
         }
+
+        public async Task<long> StoreAsync<T>(string tableName, string identityName, T data)
+        {
+            long identityValue = 0;
+            StringBuilder sb = new StringBuilder();
+            StringBuilder suffix = new StringBuilder(); ;
+            Type t = typeof(T);
+            bool isUpdate = false;
+            var properties = t.GetProperties();
+            foreach (var property in properties)
+            {
+                if (property.Name.ToLower().CompareTo(identityName.ToLower()) == 0)
+                {   // id
+
+                    object x = property.GetValue(data);
+                    identityValue = Convert.ToInt64(x);
+                    if (identityValue > 0)
+                    {   // this is an update.
+                        isUpdate = true;
+                        suffix.Append($" where {identityName} = {identityValue}");
+                    }
+                    else
+                    {   // this is an insert.
+                        isUpdate = false;
+                    }
+                }
+            }
+            if (isUpdate)
+            {
+                bool initial = true;
+                sb.Append($"update {tableName} set ");
+                foreach (var property in properties)
+                {   // update t set x=a, y=b ... | where i= z
+                    if (property.Name.ToLower().CompareTo(identityName.ToLower()) == 0)
+                    {   // don't set the identity property!
+                        continue;
+                    }
+                    if (property.PropertyType == typeof(string)
+                        || property.PropertyType == typeof(bool)
+                        || property.PropertyType == typeof(DateTime))
+                    {
+                        if (initial)
+                            sb.Append($"{property.Name}='{property.GetValue(data)}'");
+                        else
+                            sb.Append($", {property.Name}='{property.GetValue(data)}'");
+                        initial = false;
+                    }
+                    else
+                    {
+                        if (initial)
+                            sb.Append($"{property.Name}={property.GetValue(data).ToString()}");
+                        else
+                            sb.Append($", {property.Name}={property.GetValue(data).ToString()}");
+                        initial = false;
+                    }
+                }
+                sb.Append(suffix);
+            }
+            else
+            {   // insert into t (x, y, z) values | (a, b, c)
+                bool initial = true;
+                sb.Append($"insert into {tableName} (");
+                foreach (var property in properties)
+                {
+                    if (property.Name.ToLower().CompareTo(identityName.ToLower()) == 0)
+                    {   // don't set the identity property!
+                        continue;
+                    }
+                    if (property.PropertyType == typeof(string)
+                        || property.PropertyType == typeof(bool)
+                        || property.PropertyType == typeof(DateTime))
+                    {
+                        if (initial)
+                        {
+                            sb.Append($"{property.Name}");
+                            suffix.Append($"'{property.GetValue(data)}'");
+                        }
+                        else
+                        {
+                            sb.Append($", {property.Name}");
+                            suffix.Append($", '{property.GetValue(data)}'");
+                        }
+                        initial = false;
+                    }
+                    else
+                    {
+                        if (initial)
+                        {
+                            sb.Append($"{property.Name}");
+                            suffix.Append($"{property.GetValue(data).ToString()}");
+                        }
+                        else
+                        {
+                            sb.Append($", {property.Name}");
+                            suffix.Append($", {property.GetValue(data).ToString()}");
+                        }
+                        initial = false;
+                    }
+                }
+                sb.Append($") output inserted.{identityName} values (");
+                sb.Append(suffix);
+                sb.Append(")");
+            }
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(sb.ToString(), connection);
+                command.Connection.Open();
+                if (isUpdate)
+                {
+                    await command.ExecuteNonQueryAsync();
+                    return identityValue;
+                }
+                else // is insert
+                {
+                    object x = await command.ExecuteScalarAsync();
+                    return Convert.ToInt64(x);
+                }
+            }
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -178,22 +316,93 @@ namespace WorkflowManager.Services
             }
             return x;
         }
+        public async Task<T[]> RecallAsync<T>(string tableName, string identityName, long identity)
+        {
+            T[] x = new T[0];
+            string sql = $"select * from {tableName} where {identityName}={identity} for json path";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Connection.Open();
+                var result = await command.ExecuteReaderAsync();
+                while (result.Read())
+                {
+                    var j0 = result.GetString(0);
+                    x = JsonConvert.DeserializeObject<T[]>(j0);
+                }
+            }
+            return x;
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="tableName"></param>
-        /// <param name="whereClause"></param>
+        /// <param name="condition"></param>
         /// <returns></returns>
-        public T[] Recall<T>(string tableName, string whereClause)
+        public T[] Recall<T>(string tableName, string condition)
         {
             T[] x = new T[0];
-            string sql = $"select * from {tableName} where {whereClause} for json path";
+            string sql = $"select * from {tableName} where {condition} for json path";
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 SqlCommand command = new SqlCommand(sql, connection);
                 command.Connection.Open();
                 var result = command.ExecuteReader();
+                while (result.Read())
+                {
+                    var j0 = result.GetString(0);
+                    x = JsonConvert.DeserializeObject<T[]>(j0);
+                }
+            }
+            return x;
+        }
+        public async Task<T[]> RecallAsync<T>(string tableName, string condition)
+        {
+            T[] x = new T[0];
+            string sql = $"select * from {tableName} where {condition} for json path";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Connection.Open();
+                var result = await command.ExecuteReaderAsync();
+                while (result.Read())
+                {
+                    var j0 = result.GetString(0);
+                    x = JsonConvert.DeserializeObject<T[]>(j0);
+                }
+            }
+            return x;
+        }
+        public T[] Recall<T>(string sql)
+        {
+            T[] x = new T[0];
+            if (!sql.ToLower().Contains("json"))
+                sql += " for json path";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Connection.Open();
+                var result = command.ExecuteReader();
+                while (result.Read())
+                {
+                    var j0 = result.GetString(0);
+                    x = JsonConvert.DeserializeObject<T[]>(j0);
+                }
+            }
+            return x;
+        }
+
+        public async Task<T[]> RecallAsync<T>(string sql)
+        {
+            T[] x = new T[0];
+            if (!sql.ToLower().Contains("json"))
+                sql += " for json path";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Connection.Open();
+                var result = await command.ExecuteReaderAsync();
                 while (result.Read())
                 {
                     var j0 = result.GetString(0);
